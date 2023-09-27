261ae8e1bf0a9a2decd94d7196b336f41e1c1231
nikiopia <124698022+nikiopia@users.noreply.github.com>
2023-09-27 16:14:44 -0400

    Add files via upload

--- cl-strlib.c
+++ cl-strlib.c
@@ -6,34 +6,24 @@
 
 #define MAX_ITER 100
 
-/*!
- * @function	cl_strlen
- * @param		str			Source string
- * @result		Returns the length of a string
-**/
-int cl_strlen (char str[]) {
-	if (!str) { return -1; }
+int cl_strlen (char str[], int* result) {
+	if (!str) { return -1; } // existence check
 	char* i = &str[0];
-	// some protection against missing null terminators
+	// MAX_ITER for some protection against missing null terminators ('\0')
 	while (*i != '\0' && (i - &str[0] + 1) < MAX_ITER) {
 		i++;
 	}
 	
-	return (i - &str[0]);
+	if (*i != '\0' && (i - &str[0]) == MAX_ITER) {
+		// the token case when it is stopped by MAX_ITER
+		*result = MAX_ITER;
+		return -1;
+	}
+	
+	*result = (i - &str[0]);
+	return 0;
 }
 
-/*!
- * @function	c1_substr
- * @discussion	Requires result string is already null-terminated,
- *				 0 -> success
- *				-1 -> source and/or result don't exist
- *				-2 -> starting index out of range
- * @param		source		Source string
- * @param		result		Where the substring is saved
- * @param		startIndex	Starting index of substring
- * @param		length		Length of substring
- * @result		A substring of the input string
-**/
 int cl_substr (char str[], char result[], int startIndex, int length) {
 	if (!result || !str) { return -1; }
 	if (startIndex < 0 || length < 0) { return -2; }
@@ -42,10 +32,14 @@ int cl_substr (char str[], char result[], int startIndex, int length) {
 	char* i = &str[0];
 	char* j = &result[0];
 	
-	while (*i != '\0' && (i - &str[0]) < startIndex) { i++; }
+	// get to startIndex
+	while (*i != '\0' && (i - &str[0]) < startIndex) {
+		i++;
+	}
 	
-	if (*i == '\0') { return -2; }
+	if (*i == '\0') { return -2; } // couldn't get there
 	
+	// (i - &str[0]) is the current index
 	while (*i != '\0' && (i - &str[0]) >= startIndex && (i - &str[0]) <= endIndex) {
 		if (*j != '\0') {
 			*j = *i;
@@ -57,20 +51,6 @@ int cl_substr (char str[], char result[], int startIndex, int length) {
 	return 0;
 }
 
-/*!
- * @function	cl_hstint
- * @discussion	Requires '0x' prefix on numbers, since
- *				they are hex values. Stops converting
- *				at first non hex digit
- *				 0 -> success
- *				-1 -> str and/or result don't exist
- *				-2 -> length error (length < 3)
- *				-3 -> missing 0x prefix
- * @param		str			Source string
- * @param		result		Converted value as an int
- * @result		Takes in a string of a hex number and returns
- *				by reference its equivalent value as an int
-**/
 int cl_hstint (char str[], int* result) {
 	if (!str || !result) { return -1; } // existence check
 	if (cl_strlen(str) < 3) { return -2; } // too small to hold information
@@ -81,7 +61,7 @@ int cl_hstint (char str[], int* result) {
 	int keepLooping = 1;
 	int value;
 	while (*i != '\0' && keepLooping) {
-		value = (*(int*) i) & 0xFF;
+		value = (*(int*) i) & 0xFF; // get character at i as if it's an int
 		if (value >= 48 && value <= 57) {
 			// 0-9
 			value = value - 48;
@@ -92,7 +72,7 @@ int cl_hstint (char str[], int* result) {
 			// A-F
 			value = value - 55;
 		} else {
-			// who knows
+			// who knows what this is, dont do anything and stop
 			value = -1;
 			keepLooping = 0;
 		}
@@ -106,13 +86,6 @@ int cl_hstint (char str[], int* result) {
 	return 0;
 }
 
-/*!
- * @function	cl_strtest
- * @discussion	Creates char[5] and tests each element
- *				for equality with the null byte, printing
- *				results as it tests
- * @result		Void
-**/
 void cl_strtest (void) {
 	char test[5];
 	for (int i = 0; i < 5; ++i) {
--- cl-strlib.h
+++ cl-strlib.h
@@ -1,10 +1,54 @@
 #ifndef CL_STRLIB_H
 #define CL_STRLIB_H
+
 #include "cl-strlib.c"
 
+/*!
+ * @function	cl_strlen
+ * @discussion	 0 -> success
+ *				-1 -> stopped by MAX_ITER
+ * @param		str			Source string
+ * @param		result		Integer length returned by reference
+ * @result		Returns the length of a string
+**/
 int cl_strlen(char* str);
+
+/*!
+ * @function	c1_substr
+ * @discussion	Requires result string is already null-terminated,
+ *				 0 -> success
+ *				-1 -> source and/or result don't exist
+ *				-2 -> starting index out of range
+ * @param		source		Source string
+ * @param		result		Where the substring is saved
+ * @param		startIndex	Starting index of substring
+ * @param		length		Length of substring
+ * @result		A substring of the input string
+**/
 int cl_substr(char* str, char* result, int startIndex, int length);
+
+/*!
+ * @function	cl_hstint
+ * @discussion	Requires '0x' prefix on numbers, since
+ *				they are hex values. Stops converting
+ *				at first non hex digit after the prefix
+ *				 0 -> success
+ *				-1 -> str and/or result don't exist
+ *				-2 -> length error (length < 3)
+ *				-3 -> missing 0x prefix
+ * @param		str			Source string
+ * @param		result		Converted value as an int
+ * @result		Takes in a string of a hex number and returns
+ *				by reference its equivalent value as an int
+**/
 int cl_hstint(char* str, int* result);
 
+/*!
+ * @function	cl_strtest
+ * @discussion	Creates char[5] and tests each element
+ *				for equality with the null byte, printing
+ *				results as it tests
+ * @result		Void
+**/
 void cl_strtest(void);
 #endif
